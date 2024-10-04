e82a2e0ffb7200628c1c4d410d4715033bb0b038
Takashi Yamamoto <tyamamoto@makoshu.com>
2024-10-04 16:21:30 -0400

    Update to logging

--- rds.js
+++ rds.js
@@ -16,7 +16,7 @@ function rds() {
   const secret = JSON.parse(config.secret.db_secret);
   rdsUrl = secret.host;
   console.log(`Connecting to RDS at ${rdsUrl}`);
-  console.log(process.env);
+  console.log(JSON.stringify(process.env,null,'\t'));
   
   rdsPool = mysql.createPool({
     connectionLimit : 12,
